3741b81701dd9ea164908a4897dce011a8716cac
jeffinfrancis <jeffin2202@gmail.com>
2021-04-12 14:51:30 -0700

    Add project files.

---
+++ WebApplication1/Controllers/SVMonitorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SVMonitorController : ControllerBase
+    {
+        // GET: api/<SVMonitorController>
+        [HttpGet]
+        public IEnumerable<Plants> Get()
+        {
+            return new PlantsData().GetPlants();
+        }
+
+        // GET api/<SVMonitorController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SVMonitorController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SVMonitorController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SVMonitorController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Model/Plants.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    public class Plants
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PlantName { get; set; }
+        public DateTime LastWatered { get; set; }
+        public string ImageUrl { get; set; }
+        public string WaterStatus { get; set; }
+    }
+
+    public class PlantsData
+    {
+        public Plants[] GetPlants()
+        {
+            Plants[] PlantsList = new Plants[] {
+        new Plants{
+            Id=1,
+        LastWatered=DateTime.Now.ToUniversalTime(),
+        PlantName="Hibiscus",
+        ImageUrl="/Images/plant1.jpg",
+         WaterStatus=""
+        },
+         new Plants{
+            Id=2,
+        LastWatered=DateTime.Now.ToUniversalTime(),
+        PlantName="Rose",
+         ImageUrl="/Images/plant2.jpg",
+          WaterStatus=""
+        },
+          new Plants{
+            Id=3,
+        LastWatered=DateTime.Now.ToUniversalTime(),
+        PlantName="Jasmine",
+         ImageUrl="/Images/plant3.jpg",
+          WaterStatus=""
+        },
+           new Plants{
+            Id=4,
+        LastWatered=DateTime.Now.ToUniversalTime(),
+        PlantName="Sunflower",
+         ImageUrl="/Images/plant1.jpg",
+          WaterStatus=""
+        },
+            new Plants{
+            Id=5,
+        LastWatered=DateTime.Now.ToUniversalTime(),
+        PlantName="Chrysanthimum",
+         ImageUrl="/Images/plant3.jpg",
+         WaterStatus=""
+        }
+        };
+            return PlantsList;
+        }
+        
+    }
+}
